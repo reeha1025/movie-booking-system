32a0e4faf43bd51fd832a2787f55b3cc891e3404
SHAIK REHANA <rehaashaik06@gmail.com>
2025-12-05 15:23:58 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "Building Django application for production..."
-
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --noinput
 
-# Run migrations
-python manage.py migrate --noinput
-
-echo "Build completed successfully!"
