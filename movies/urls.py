3e42b2708400f238d33e770ab5ce7253677526fb
SHAIK REHANA <rehaashaik06@gmail.com>
2025-12-05 06:14:35 +0530

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.movie_list,name='movie_list'),
+
+urlpatterns = [
+    path('', views.movie_list, name='movie_list'),
     path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
-    path('<int:movie_id>/theaters',views.theater_list,name='theater_list'),
-    path('theater/<int:theater_id>/seats/book/',views.book_seats,name='book_seats'),
-    path('theater/<int:theater_id>/checkout/',views.checkout,name='checkout'),
-    path('bookings/<int:booking_id>/pay/',views.pay_booking,name='pay_booking'),
-    path('bookings/<int:booking_id>/otp/<str:upi_app>/',views.upi_otp,name='upi_otp'),
-    path('bookings/<int:booking_id>/scanner/',views.upi_scanner,name='upi_scanner'),
-    path('bookings/<int:booking_id>/payment-success/',views.payment_success,name='payment_success'),
-    path('bookings/<int:booking_id>/cancel/',views.cancel_booking,name='cancel_booking'),
+    path('<int:movie_id>/theaters', views.theater_list, name='theater_list'),
+    path('theater/<int:theater_id>/seats/book/', views.book_seats, name='book_seats'),
+    path('theater/<int:theater_id>/checkout/', views.checkout, name='checkout'),
+    path('bookings/<int:booking_id>/pay/', views.pay_booking, name='pay_booking'),
+    path('bookings/<int:booking_id>/otp/<str:upi_app>/', views.upi_otp, name='upi_otp'),
+    path('bookings/<int:booking_id>/scanner/', views.upi_scanner, name='upi_scanner'),
+    path('bookings/<int:booking_id>/payment-success/', views.payment_success, name='payment_success'),
+    path('bookings/<int:booking_id>/cancel/', views.cancel_booking, name='cancel_booking'),
 ]
