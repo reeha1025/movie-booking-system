2753502dd2b6162a54734397a80ce27c97f7ead8
SHAIK REHANA <rehaashaik06@gmail.com>
2025-12-05 12:20:19 +0530

    Update urls.py

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -13,3 +13,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("movies.urls")),   # ADD THIS
+]
+
