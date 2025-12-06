e708dbaf15501c07e132e0fdf7543885d2414df0
SHAIK REHANA <rehaashaik06@gmail.com>
2025-12-06 16:14:25 +0530

    Update urls.py

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -3,20 +3,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from movies.views import analytics_dashboard
+
 urlpatterns = [
+    # Admin
     path('secure-admin-panel-bms2025/', admin.site.urls),
+
+    # Users app
     path('users/', include('users.urls')),
-    path('',include('users.urls')),
-    path('movies/', include('movies.urls')),
+
+    # Movies app (Homepage)
+    path('', include('movies.urls')),   # 👈 THIS FIXES YOUR 404
+
+    # Analytics
     path('secure-analytics-bms2025/', analytics_dashboard, name='analytics_dashboard'),
 ]
 
+# Media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("movies.urls")),   # ADD THIS
-]
 
