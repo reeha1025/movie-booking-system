7329197c24bc6081644d38423a59b710f6fb9a02
SHAIK REHANA <rehaashaik06@gmail.com>
2025-12-06 20:48:04 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # STATIC FILES (Whitenoise Required)
 # -------------------------------
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # -------------------------------
